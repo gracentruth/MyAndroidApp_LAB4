3760d31a3bbd985da537690fce0cc55ea70543c4
gracentruth <63465350+gracentruth@users.noreply.github.com>
2021-07-08 19:42:41 +0900

    Implement codlab7.3

--- app/src/main/java/com/example/myandroidapp_lab4/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp_lab4/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.myandroidapp_lab4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver,
+                        new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(customBroadcastIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //Unregister the receiver
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
